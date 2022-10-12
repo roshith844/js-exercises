8e8d17c01d869238bfae04062c9dca87fc2d863d
Roshith P <roshith844@gmail.com>
2022-10-12 17:32:01 +0530

    Update : Assignment 6 input convert to number 1st.

--- js-files/a6.js
+++ js-files/a6.js
@@ -1,32 +1,32 @@
 let num = prompt("Enter a Number")
-if(isNaN(Number(num))){
+num = Number(num)
+if (isNaN(num)) {
      console.log("invalid! Both Values should be a Number")
-}else{
-     num = Number(num)
-     switch(num){
-case 1: 
-console.log("Sunday")
-break;
-case 2: 
-console.log("Monday")
-break;
-case 3: 
-console.log("Tuesday")
-break;
-case 4: 
-console.log("wednesday")
-break;
-case 5: 
-console.log("Thursday")
-break;
-case 6: 
-console.log("Friday")
-break;
-case 7: 
-console.log("Saturday")
-break;
-default: 
-console.log("invalid Entry")
+} else {
+     switch (num) {
+          case 1:
+               console.log("Sunday")
+               break;
+          case 2:
+               console.log("Monday")
+               break;
+          case 3:
+               console.log("Tuesday")
+               break;
+          case 4:
+               console.log("wednesday")
+               break;
+          case 5:
+               console.log("Thursday")
+               break;
+          case 6:
+               console.log("Friday")
+               break;
+          case 7:
+               console.log("Saturday")
+               break;
+          default:
+               console.log("invalid Entry")
      }
 }
 
