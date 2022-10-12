50923362734d8557259271a746490e011bc36825
Roshith P <roshith844@gmail.com>
2022-10-12 16:19:15 +0530

    assignment 3

---
+++ js-files/a3.js
@@ -0,0 +1,4 @@
+let amount = prompt("Enter Principle Amount");
+let rate = prompt("Enter Interest Rate");
+let years = prompt("Enter number of years ");
+console.log((Number(amount)*Number(rate)*Number(years))/100);
