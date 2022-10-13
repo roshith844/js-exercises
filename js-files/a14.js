0b1a3076c570685e443055b4e759e8c732a10cce
Roshith P <roshith844@gmail.com>
2022-10-13 20:20:49 +0530

    A14 : define arrays

---
+++ js-files/a14.js
@@ -0,0 +1,16 @@
+let size = Number(prompt("Enter Size of Arrays"))
+// defines arrays
+let array1 = new Array(size)
+for(let i = 0; i<size; i++){
+     array1[i]= new Array(size)
+}
+let array2 = new Array(size)
+for(let i = 0; i<size; i++){
+     array2[i]= new Array(size)
+}
+let sum = new Array(size)
+for(let i = 0; i<size; i++){
+     sum[i]= new Array(size)
+}
+
+ 
