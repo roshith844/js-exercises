9d1252e45eabb876a6914f1b1e8684742f7380f3
Roshith P <roshith844@gmail.com>
2022-10-13 22:56:49 +0530

    assignment 15 :  tested OK

--- js-files/a14.js
+++ js-files/a14.js
@@ -1,35 +1,35 @@
 let size = Number(prompt("Enter Size of Arrays"))
 // defines arrays
 let array1 = new Array(size)
-for(let i = 0; i<size; i++){
-     array1[i]= new Array(size)
+for (let i = 0; i < size; i++) {
+     array1[i] = new Array(size)
 }
 let array2 = new Array(size)
-for(let i = 0; i<size; i++){
-     array2[i]= new Array(size)
+for (let i = 0; i < size; i++) {
+     array2[i] = new Array(size)
 }
 let sum = new Array(size)
-for(let i = 0; i<size; i++){
-     sum[i]= new Array(size)
+for (let i = 0; i < size; i++) {
+     sum[i] = new Array(size)
 }
 // takes input
 console.log("enter values to Array 1 : ")
-for(let i=0; i<size; i++){
-     for(let j=0; j<size; j++){
+for (let i = 0; i < size; i++) {
+     for (let j = 0; j < size; j++) {
           array1[i][j] = Number(prompt("enter element"))
      }
 }
 
 console.log("enter values to Array 2 : ")
-for(let i=0; i<size; i++){
-     for(let j=0; j<size; j++){
+for (let i = 0; i < size; i++) {
+     for (let j = 0; j < size; j++) {
           array2[i][j] = Number(prompt("enter element"))
      }
 }
 
 //sum array1 and array2
-for(let i=0; i<size; i++){
-     for(let j=0; j<size; j++){
+for (let i = 0; i < size; i++) {
+     for (let j = 0; j < size; j++) {
           sum[i][j] = array1[i][j] + array2[i][j]
      }
 }
@@ -39,4 +39,4 @@ console.table(sum)
 
 
 
- 
+
---
+++ js-files/a15.js
@@ -0,0 +1,24 @@
+let size = prompt("Enter size of the array")
+main(size)
+//main function
+function main(size) {
+     let array = new Array(size)
+
+     array = getArray(array, size)
+
+     displayArray(array, size)
+}
+//adds elements to arrays from user
+function getArray(array, size) {
+     console.log("enter array elements")
+     for (i = 0; i < size; i++) {
+          array[i] = prompt("enter array element")
+     }
+     return array;
+}
+//displays array
+function displayArray(array, size) {
+     console.log(array)
+
+
+}
