72768a2ee945d6292471651704016682b36fd35b
Roshith P <roshith844@gmail.com>
2022-10-13 22:23:34 +0530

    assignment 14 Test Ok

--- js-files/a14.js
+++ js-files/a14.js
@@ -12,5 +12,31 @@ let sum = new Array(size)
 for(let i = 0; i<size; i++){
      sum[i]= new Array(size)
 }
+// takes input
+console.log("enter values to Array 1 : ")
+for(let i=0; i<size; i++){
+     for(let j=0; j<size; j++){
+          array1[i][j] = Number(prompt("enter element"))
+     }
+}
+
+console.log("enter values to Array 2 : ")
+for(let i=0; i<size; i++){
+     for(let j=0; j<size; j++){
+          array2[i][j] = Number(prompt("enter element"))
+     }
+}
+
+//sum array1 and array2
+for(let i=0; i<size; i++){
+     for(let j=0; j<size; j++){
+          sum[i][j] = array1[i][j] + array2[i][j]
+     }
+}
+//displays sum
+console.table(sum)
+
+
+
 
  
