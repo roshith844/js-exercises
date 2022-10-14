a8a4293755d492452fe82d6f4b9c8cbf51479bc4
Roshith P <roshith844@gmail.com>
2022-10-14 19:44:08 +0530

    22 Test OK

--- js-files/a22.js
+++ js-files/a22.js
@@ -5,22 +5,39 @@ function main(){
      console.log("Enter Values of array 1")
      let array1 = getArray(size)
      console.log("Enter Values of array 2")
-     let array2 = getArray(size) 
+     let array2 = getArray(size)
      console.log("sum of array 1 and 2 is :")
-     let sum =addArray(array1, array2)
+     let sum =addArray(array1, array2,size)
      displayArray(sum)
 }
+
 function getArray(size){
-     array= []
-     for(let i=0; i<size ; i++){
-          array[i]= prompt("Enter Value")
+     //defines array
+     let array = new Array(size)
+     for (let i = 0; i < size; i++) {
+          array[i] = new Array(size)
      }
-     return array;
+     // takes input
+console.log("enter values to Array : ")
+for (let i = 0; i < size; i++) {
+     for (let j = 0; j < size; j++) {
+          array[i][j] = Number(prompt("enter element"))
+     }
 }
-function addArray(arr1, arr2){
-          sum=[]
+return array;
+
+}
+function addArray(arr1, arr2,size){
+     let sum = new Array(size)
+     for (let i = 0; i < size; i++) {
+          sum[i] = new Array(size)
+     }
+
           for(let i=0; i< size; i++){
-               sum[i]= arr1[i] + arr2[i]
+               for(let j=0; j<size; j++){
+                    sum[i][j]= arr1[i][j] + arr2[i][j]
+               }
+               
           }
           return sum
 }
