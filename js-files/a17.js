569d0debdf4fcccf3f1ce6d4983616b6e18adc61
Roshith P <roshith844@gmail.com>
2022-10-15 11:32:19 +0530

    Update: modify assignment 17 to Oops

--- js-files/a17.js
+++ js-files/a17.js
@@ -1,27 +1,38 @@
 const num1 = Number(prompt("Enter a Number"))
 const num2 = Number(prompt("Enter Another Number"))
 let choice = Number(prompt("Enter Your choice : 1. addition 2. substraction 3. multiplication 4. division"))
-if(choice < 1 || choice > 4 ){
+class Operations {
+     constructor(num1, num2) {
+          this.num1 = num1
+          this.num2 = num2
+     }
+     addition() {
+          console.log(this.num1 + this.num2)
+     }
+     subtraction() {
+          console.log(this.num1 - this.num2)
+     }
+     multiplication() {
+          console.log(this.num1 + this.num2)
+     }
+     division() {
+          console.log(this.num1 / this.num2)
+     }
+
+}
+let operation = new Operations(num1, num2)
+if (choice < 1 || choice > 4) {
      console.log("Invalid Entry")
-}else if(choice === 1){
-     addition(num1, num2)
-}else if(choice === 2){
-     subtraction(num1, num2)
-}else if(choice === 3){
-     multiplication(num1, num2)
-}else if(choice === 4){
-     division(num1, num2)
+} else if (choice === 1) {
+     operation.addition()
+} else if (choice === 2) {
+     operation.subtraction()
+} else if (choice === 3) {
+     operation.multiplication()
+} else if (choice === 4) {
+     operation.division()
 }
 
-function addition(num1, num2){
-     console.log(num1 + num2)
-}
-function subtraction(num1, num2){
-    console.log(num1 - num2)
-}
-function multiplication(num1, num2) {
-     console.log (num1 + num2)
-}
-function division(num1, num2) {
-     console.log(num1 / num2)
-}
+
+
+
