f96701f57ac78973d013b1573f88113c93ff3bfb
Roshith P <roshith844@gmail.com>
2022-10-17 10:10:31 +0530

    25 Test OK

---
+++ js-files/a25.js
@@ -0,0 +1,26 @@
+var library = [ 
+     {
+         title: 'Bill Gates',
+         author: 'The Road Ahead',
+         readingStatus: true
+     },
+     {
+         title: 'Steve Jobs',
+         author: 'Walter Isaacson',
+         readingStatus: true
+     },
+     {
+         title: 'Mockingjay: The Final Book of The Hunger Games',
+         author: 'Suzanne Collins',
+         readingStatus: false
+     }
+ ];
+
+const len = library.length
+for(let i=0; i< library.length; i++){
+     if(library[i].readingStatus === false){
+          console.log(`You still need to read ${library[i].title} by ${library[i].author}`)
+     }else{
+          console.log(`Already read ${library[i].title} by ${library[i].author}.`)
+     }
+}
