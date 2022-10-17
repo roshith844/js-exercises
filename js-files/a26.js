86c71a71ed0dbe5bcc51131b815c10e12b66fe10
Roshith P <roshith844@gmail.com>
2022-10-17 16:53:16 +0530

    Update : put ' ' to split and  join methods

--- js-files/a26.js
+++ js-files/a26.js
@@ -1,6 +1,6 @@
 let my_string = Number(1234)
 try{
-     let revstring = my_string.split().reverse().join()
+     let revstring = my_string.split('').reverse().join('')
      console.log(`Reversed string is : ${my_string}`) 
 }catch(err){
      console.log(`Error : ${err.message}`)
