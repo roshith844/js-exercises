f2d3faabe63578cb237516f6478e42005365c8ce
Roshith P <roshith844@gmail.com>
2022-12-30 14:55:49 +0530

    chore: spelling correction

--- js-files/basic-array-operations.js
+++ js-files/basic-array-operations.js
@@ -2,5 +2,5 @@ const arr = [10, 28,7,4,8,8]
 arr.push(10) // adds element to last
 arr.pop() // removes element from last  
 arr.unshift(150) // adds element on first
-arr.shift() // remove element from first
+arr.shift() // removes element from first
 console.log(arr)
