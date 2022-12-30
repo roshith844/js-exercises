4519dd9cd33ee6acab89ffe0311d571510c1a1b9
Roshith P <roshith844@gmail.com>
2022-12-30 14:55:05 +0530

    example: basic array operations

---
+++ js-files/basic-array-operations.js
@@ -0,0 +1,6 @@
+const arr = [10, 28,7,4,8,8]
+arr.push(10) // adds element to last
+arr.pop() // removes element from last  
+arr.unshift(150) // adds element on first
+arr.shift() // remove element from first
+console.log(arr)
