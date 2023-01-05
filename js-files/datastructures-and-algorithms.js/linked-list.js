0ab8d56732c1a8a66acc56d2da35ab5bf196e62f
Roshith P <roshith844@gmail.com>
2023-01-05 11:32:57 +0530

    construct linked list and perform basic operations

---
+++ js-files/datastructures-and-algorithms.js/linked-list.js
@@ -0,0 +1,128 @@
+// construction of node
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+const node1 = new Node(100);
+console.log(node1);
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+
+  // Insert first node
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+    this.size++;
+  }
+
+  // Insert last node
+  insertLast(data) {
+    let node = new Node(data);
+    let current;
+    // if empty make it head
+    if (!this.head) {
+      this.head = node;
+    } else {
+      current = this.head;
+      while (current.next) {
+        current = current.next;
+      }
+      current.next = node;
+    }
+    this.size++;
+  }
+
+  // Insert at index
+  insertAt(data, index) {
+    // if index is out of range
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    if (index === 0) {
+      this.head = new Node(data, this.head);
+      return;
+    }
+
+    const node = new Node(data);
+    let current, previous;
+
+    // Set current to first
+    current = this.head;
+    let count = 0;
+    while (count < index) {
+      previous = current; // Node before index
+      count++;
+      current = current.next; // Node after Index
+    }
+    node.next = current;
+    previous.next = node;
+    this.size++;
+  }
+  // Get at index
+  getAt(index) {
+    let current = this.head;
+    let count = 0;
+
+    while (current) {
+      if (count == index) {
+        console.log(current.data);
+      }
+      count++;
+      current = current.next;
+    }
+    return null;
+  }
+  // Remove at index
+  removeAt(index) {
+    if (index > 0 && index > this.size) {
+      return;
+    }
+    let current = this.head;
+    let previous;
+    let count = 0;
+
+    // remove first
+    if (index === 0) {
+      this.head = current.next;
+    } else {
+      while (count < index) {
+        count++;
+        previous = current;
+        current = current.next;
+      }
+      previous.next - current.next;
+    }
+    previous.next = current.next;
+  }
+  // Clear List
+  clearList() {
+    this.head = null;
+    this.size = 0;
+  }
+  // Print list
+  printListData() {
+    let current = this.head;
+    while (current) {
+      console.log(current.data);
+      current = current.next;
+    }
+  }
+}
+
+const ll = new LinkedList();
+ll.insertFirst(100);
+ll.insertFirst(200);
+ll.insertFirst(400);
+ll.insertLast(1000);
+ll.insertAt(500, 2);
+ll.removeAt(1);
+ll.clearList();
+console.log(ll);
+ll.printListData();
+// ll.getAt(1)
