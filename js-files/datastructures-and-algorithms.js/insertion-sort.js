65f3ec0dbadf8690ebbabb0d63c8e8505b135aa9
ROSHITH P <roshith844@gmail.com>
2023-04-04 19:33:48 +0530

    example: insertion sort

---
+++ js-files/datastructures-and-algorithms.js/insertion-sort.js
@@ -0,0 +1,12 @@
+const arr = [8, 4, 3, 5, 7, 6]
+for (let i = 1; i < arr.length; i++) {
+    let currentElement = arr[i]
+    let j = i - 1
+
+    while (j >= 0 && arr[j] > currentElement) {
+        arr[j + 1] = arr[j]
+        j--
+    }
+    arr[j + 1] = currentElement
+}
+console.log(arr)
