b6c4f0c9e971841ec7ea43b141de1aba678a32b7
ROSHITH P <roshith844@gmail.com>
2023-04-25 14:34:02 +0530

    problem:  easy leet code question

--- js-files/problems/array.js
+++ js-files/problems/array.js
@@ -45,3 +45,24 @@ function reverseArray(arrayInput){
    return arrayInput;
 }
 reverseArray([1, 2, 3, 4, 5])
+
+// 4. You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+
+// Increment the large integer by one and return the resulting array of digits.
+
+function plusOne(digits) {
+    let n = digits.length;
+    for (let i = n - 1; i >= 0; i--) {
+        if (digits[i] === 9) {
+            digits[i] = 0;
+        } else {
+            digits[i]++;
+            return digits;
+        }
+    }
+    // If we reach here, it means that all digits were 9
+    digits.unshift(1);
+    return digits;
+}
+
+console.log(plusOne([8, 8, 9]))
