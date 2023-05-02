0098d66328163b7d3c313f734ee831a81b26f0ce
ROSHITH P <roshith844@gmail.com>
2023-05-02 18:25:19 +0530

    example: dom change color using dom

---
+++ dom/change-color.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('#button')
+button.addEventListener('click', ()=>{
+    document.getElementById('box').style.backgroundColor = 'red'
+})
+
+button.addEventListener('mouseover', ()=>{
+    document.getElementById('box').style.backgroundColor = 'yellow'
+})
